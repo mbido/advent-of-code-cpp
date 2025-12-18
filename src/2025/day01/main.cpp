d03baf486fc7e58df129925e40fed8d370ba20bb
mbido <ma.bidault39@gmail.com>
2025-12-18 15:53:21 +0100

    some updates + solving 2025 day 01

--- src/2025/day01/main.cpp
+++ src/2025/day01/main.cpp
@@ -1,44 +1,71 @@
 #include "utils.hpp"
-#include <iostream>
 #include <chrono>
+#include <iostream>
 #include <string>
 
-void solve(const char* filename, bool silent = false) {
-    aoc::MappedFile input(filename);
-    const char* p = input.begin();
-    const char* end = input.end();
+void solve(const char *filename, bool silent = false) {
+  aoc::MappedFile input(filename);
+  const char *p = input.begin();
+  const char *end = input.end();
 
-    long long part1 = 0;
-    long long part2 = 0;
+  long long part1 = 0;
+  long long part2 = 0;
+  int current_pos = 50;
 
-    // TODO: Implementation
-    
-    if (!silent) {
-        std::cout << "Part 1: " << part1 << "\n";
-        std::cout << "Part 2: " << part2 << "\n";
+  while (p < end) {
+    while (p < end && *p != 'L' && *p != 'R')
+      p++;
+    if (p >= end)
+      break;
+
+    char dir = *p++;
+    long long dist = aoc::fast_atoi<long long>(p);
+
+    if (dir == 'L') {
+      int dist_to_zero = (current_pos == 0) ? 100 : current_pos;
+      if (dist >= dist_to_zero) {
+        part2 += (dist - dist_to_zero) / 100 + 1;
+      }
+      current_pos = (current_pos - (dist % 100) + 100) % 100;
+    } else {
+      int dist_to_zero = (current_pos == 0) ? 100 : (100 - current_pos);
+      if (dist >= dist_to_zero) {
+        part2 += (dist - dist_to_zero) / 100 + 1;
+      }
+      current_pos = (current_pos + dist) % 100;
     }
+
+    if (current_pos == 0) {
+      part1++;
+    }
+  }
+
+  if (!silent) {
+    std::cout << "Part 1: " << part1 << "\n";
+    std::cout << "Part 2: " << part2 << "\n";
+  }
 }
 
-int main(int argc, char** argv) {
-    std::string filename = "data/2025/day01.txt";
-    bool run_bench = true;
+int main(int argc, char **argv) {
+  std::string filename = "data/2025/day01.txt";
+  bool run_bench = true;
 
-    for (int i = 1; i < argc; ++i) {
-        std::string_view arg(argv[i]);
-        if (arg == "-test") {
-            filename = "data/2025/test01.txt";
-        } else if (arg == "-nobench") {
-            run_bench = false;
-        }
+  for (int i = 1; i < argc; ++i) {
+    std::string_view arg(argv[i]);
+    if (arg == "-test") {
+      filename = "data/2025/test01.txt";
+    } else if (arg == "-nobench") {
+      run_bench = false;
     }
+  }
 
-    std::cout << "--- Running on: " << filename << " ---\n";
-    std::cout << "--- Results ---\n";
-    solve(filename.c_str(), false);
-    
-    if (run_bench) {
-        aoc::run_benchmark([&](bool s){ solve(filename.c_str(), s); }, 1000);
-    }
-    
-    return 0;
+  std::cout << "--- Running on: " << filename << " ---\n";
+  std::cout << "--- Results ---\n";
+  solve(filename.c_str(), false);
+
+  if (run_bench) {
+    aoc::run_benchmark([&](bool s) { solve(filename.c_str(), s); }, 5000);
+  }
+
+  return 0;
 }
--- template/main.cpp
+++ template/main.cpp
@@ -1,44 +1,42 @@
 #include "utils.hpp"
-#include <iostream>
 #include <chrono>
+#include <iostream>
 #include <string>
 
-void solve(const char* filename, bool silent = false) {
-    aoc::MappedFile input(filename);
-    const char* p = input.begin();
-    const char* end = input.end();
+void solve(const char *filename, bool silent = false) {
+  aoc::MappedFile input(filename);
+  const char *p = input.begin();
+  const char *end = input.end();
 
-    long long part1 = 0;
-    long long part2 = 0;
+  long long part1 = 0;
+  long long part2 = 0;
 
-    // TODO: Implementation
-    
-    if (!silent) {
-        std::cout << "Part 1: " << part1 << "\n";
-        std::cout << "Part 2: " << part2 << "\n";
-    }
+  if (!silent) {
+    std::cout << "Part 1: " << part1 << "\n";
+    std::cout << "Part 2: " << part2 << "\n";
+  }
 }
 
-int main(int argc, char** argv) {
-    std::string filename = "data/dayXX.txt";
-    bool run_bench = true;
+int main(int argc, char **argv) {
+  std::string filename = "data/dayXX.txt";
+  bool run_bench = true;
 
-    for (int i = 1; i < argc; ++i) {
-        std::string_view arg(argv[i]);
-        if (arg == "-test") {
-            filename = "data/dayXX_test.txt";
-        } else if (arg == "-nobench") {
-            run_bench = false;
-        }
+  for (int i = 1; i < argc; ++i) {
+    std::string_view arg(argv[i]);
+    if (arg == "-test") {
+      filename = "data/dayXX_test.txt";
+    } else if (arg == "-nobench") {
+      run_bench = false;
     }
+  }
 
-    std::cout << "--- Running on: " << filename << " ---\n";
-    std::cout << "--- Results ---\n";
-    solve(filename.c_str(), false);
-    
-    if (run_bench) {
-        aoc::run_benchmark([&](bool s){ solve(filename.c_str(), s); }, 1000);
-    }
-    
-    return 0;
+  std::cout << "--- Running on: " << filename << " ---\n";
+  std::cout << "--- Results ---\n";
+  solve(filename.c_str(), false);
+
+  if (run_bench) {
+    aoc::run_benchmark([&](bool s) { solve(filename.c_str(), s); }, 5000);
+  }
+
+  return 0;
 }
