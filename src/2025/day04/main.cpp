d1066a674a8874b6b832ad0c0b39cfbc86c175cf
mbido <ma.bidault39@gmail.com>
2026-01-05 22:49:52 +0100

    saved

---
+++ src/2025/day04/main.cpp
@@ -0,0 +1,72 @@
+#include "utils.hpp"
+#include <chrono>
+#include <iostream>
+#include <string>
+
+void solve(const char *filename, bool silent = false) {
+  aoc::MappedFile input(filename);
+  const char *p = input.begin();
+  const char *end = input.end();
+
+  uint64_t part1 = 0;
+  uint64_t part2 = 0;
+
+  if (!silent) {
+    std::cout << "Part 1: " << part1 << "\n";
+    std::cout << "Part 2: " << part2 << "\n";
+  }
+}
+
+int main(int argc, char **argv) {
+  std::string filename = "data/2025/day04.txt";
+  bool run_bench = true;
+  bool silent = false;
+
+  for (int i = 1; i < argc; ++i) {
+    std::string_view arg(argv[i]);
+    if (arg == "-test") {
+      filename = "data/2025/test04.txt";
+    } else if (arg == "-nobench") {
+      run_bench = false;
+    } else if (arg == "-silent") {
+      silent = true;
+    }
+  }
+
+  if (!silent) {
+    std::cout << "--- Running on: " << filename << " ---\n";
+    std::cout << "--- Results ---\n";
+  }
+
+  try {
+    auto start = std::chrono::high_resolution_clock::now();
+    solve(filename.c_str(), silent);
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double, std::micro> elapsed = end - start;
+
+    if (!silent) {
+      if (run_bench) {
+        aoc::run_benchmark([&](bool s) { solve(filename.c_str(), s); }, 1000);
+      } else {
+        std::cout << "Time: " << elapsed.count() << " μs\n";
+      }
+    } else {
+      if (run_bench) {
+        auto res = aoc::measure_benchmark(
+            [&](bool s) { solve(filename.c_str(), s); }, 1000);
+        std::cout << res.best_time;
+      } else {
+        std::cout << elapsed.count();
+      }
+    }
+  } catch (...) {
+    if (!silent)
+      std::cerr << "Execution failed\n";
+    return 1;
+  }
+
+  return 0;
+}
+// 1372
+// 7922
